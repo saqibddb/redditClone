74d884e4c17f1d39accdb89756545f1f3663cd48
Saqib <saqib,dadabhoy@gmail.com>
2016-03-17 17:43:28 -0500

    Fixed controller naming conventions, added initial routes

--- app/app.js
+++ app/app.js
@@ -1,1 +1,17 @@
 var app = angular.module("reddit", ['ngMaterial', 'ui.router', 'firebase']);
+
+app.config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise("/home");
+
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "views/home.html",
+            controller: "postsCtrl"
+        })
+        .state('posts', {
+           url: "/posts",
+           templateUrl: "views/posts.html",
+           controller: "postsCtrl"
+        });
+});
